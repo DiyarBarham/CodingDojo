215da4ab0517f567c64fdc036b086c0999b7270b
DiyarBarham <diyarbarham@gmail.com>
2021-05-05 12:00:56 +0300

    this is my answer for loop basics assignment

---
+++ Python_Stack/_python/python_fundamentals/venv/for_loop_basic1.py
@@ -0,0 +1,27 @@
+#1
+for i in range(151):
+    print(i)
+#2
+for mult in range(5,1001,5):
+        print(mult)
+#3
+for num in range(1,101,1):
+        if num%5 ==0:
+            print("Coding")
+        if num%10 == 0:
+            print("Coding Dojo")
+#4
+sum =0
+for temp in range(1,500001,+2):
+    sum +=temp
+print(sum)
+#5
+for pos in range(2018,0,-4):
+    print(pos)
+#6
+lowNum =2
+highNum=10
+mul =2
+for j in range(lowNum,highNum+1):
+    if j%mul==0:
+        print(j)
