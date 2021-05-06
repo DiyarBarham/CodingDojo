51d6d246cbd2384f5910016f14176a2a95c95104
DiyarBarham <diyarbarham@gmail.com>
2021-05-06 11:34:39 +0300

    this is my answer for user assignment

--- Python_Stack/_python/python_fundamentals/venv/BankAccount.py
+++ Python_Stack/_python/python_fundamentals/venv/BankAccount.py
@@ -1,16 +1,27 @@
 class BankAccount:
-    def __init__(self, int_rate = 0.01, balance =0):
+    def __init__(self, balance =0, int_rate = 0.01):
         self.int_rate = int_rate
         self.balance = balance
 
     def deposit(self, amount):
         self.balance += amount
+        return self
 
     def withdraw(self, amount):
         self.balance -= amount
+        return self
 
     def display_account_info(self):
         print(f"account balance: {self.balance}, int_rate:{self.int_rate}")
+        return self
 
     def yield_interest(self):
-        self.balance *= self.int_rate
+        self.balance =self.balance+(self.balance* self.int_rate)
+        return self
+
+acc1 = BankAccount()
+acc2 = BankAccount()
+
+acc1.deposit(300).deposit(200).deposit(100).withdraw(200).yield_interest().display_account_info()
+acc2.deposit(1000).deposit(5000).withdraw(200).withdraw(100).withdraw(300).withdraw(100).yield_interest().display_account_info()
+
