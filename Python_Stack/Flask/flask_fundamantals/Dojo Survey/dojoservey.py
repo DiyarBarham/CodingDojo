2af7e5c9d94419acab882fe69683544947ccb7fb
DiyarBarham <diyarbarham@gmail.com>
2021-05-16 14:24:15 +0300

    this is my answer for dojo form qus

--- Python_Stack/Flask/flask_fundamantals/Dojo Survey/dojoservey.py
+++ Python_Stack/Flask/flask_fundamantals/Dojo Survey/dojoservey.py
@@ -9,14 +9,15 @@ def mm():
 def create_user():
     print("Got Post Info")
     print(request.form)
-    session['name'] = request.form['name']
-    session['loc'] = request.form['loc']
-    session['lang'] = request.form['lang']
-    session['comm'] = request.form['comm']
-    return redirect('/sh')
-@app.route('/sh')
-def index():
-    return render_template("show.html", name=session['name'], loc=session['loc'], lang=session['lang'], comm=session['comm'])
+    # session['name'] = request.form['name']
+    # session['loc'] = request.form['loc']
+    # session['lang'] = request.form['lang']
+    # session['comm'] = request.form['comm']
+    # return redirect('/sh')
+    return render_template("show.html", name=request.form["name"], loc=request.form['loc'], lang=request.form['lang'], comm=request.form['comm'])
+# @app.route('/sh')
+# def index():
+#     return render_template("show.html", name=session['name'], loc=session['loc'], lang=session['lang'], comm=session['comm'])
 
 if __name__=="__main__":   # Ensure this file is being run directly and not from a different module
     app.run(debug=True)    # Run the app in debug mode.
