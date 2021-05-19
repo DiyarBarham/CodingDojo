1cbb1d3d7a9898e79a6aa1dde97c570b8be6a54a
DiyarBarham <diyarbarham@gmail.com>
2021-05-19 14:03:40 +0300

    this is my answer for counter assgnment

--- Python_Stack/django/django_fundamentals/django_intro/time_display/fapp/urls.py
+++ Python_Stack/django/django_fundamentals/django_intro/time_display/fapp/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.meth),
-    path('time_display/', views.meth)
+    path('time_display/', views.meth),
+    path('test/', views.moh)
 ]
--- Python_Stack/django/django_fundamentals/django_intro/time_display/fapp/views.py
+++ Python_Stack/django/django_fundamentals/django_intro/time_display/fapp/views.py
@@ -1,8 +1,20 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
 from time import gmtime, strftime
 # Create your views here.
-def meth(request):
+
+
+def moh(request):
+
     context = {
         "time": strftime("%Y-%m-%d %H:%M %p", gmtime())
     }
     return render(request, 'index.html', context)
+def meth(request):
+
+    if request.method == "GET":
+
+        print("a GET request is being made to this route")
+        return render(request, "index.html")
+    if request.method == "POST":
+        print("a POST request is being made to this route")
+        return redirect("/")
