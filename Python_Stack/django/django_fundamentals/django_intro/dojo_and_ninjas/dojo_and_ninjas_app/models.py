f4e39adfb944e7daac1a176df9fda70abb69fd25
DiyarBarham <diyarbarham@gmail.com>
2021-05-23 16:49:29 +0300

    this is my answer for dojo and ninjas with templates assignment

--- Python_Stack/django/django_fundamentals/django_intro/dojo_and_ninjas/dojo_and_ninjas_app/models.py
+++ Python_Stack/django/django_fundamentals/django_intro/dojo_and_ninjas/dojo_and_ninjas_app/models.py
@@ -10,7 +10,7 @@ class dojo(models.Model):
     desc = models.CharField(max_length=255)
 
 class ninja(models.Model):
-    dojo_id = models.ForeignKey(dojo, related_name='ninjas', on_delete= models.CASCADE)
+    dojo_id = models.ForeignKey(dojo, related_name='ninjas', on_delete=models.CASCADE)
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     create_at = models.DateTimeField(auto_now_add=True)
--- Python_Stack/django/django_fundamentals/django_intro/dojo_and_ninjas/dojo_and_ninjas_app/views.py
+++ Python_Stack/django/django_fundamentals/django_intro/dojo_and_ninjas/dojo_and_ninjas_app/views.py
@@ -1,5 +1,16 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .models import dojo, ninja
 # Create your views here.
 def index(request):
-    pass
+    context = {
+        'dojos': dojo.objects.all(),
+        'ninjas': ninja.objects.all(),
+    }
+    return render(request, 'index.html', context)
+
+def ninjas(request):
+    ninja.objects.create(dojo_id=dojo.objects.get(name= request.POST['Dojo']), first_name=request.POST['fname'], last_name=request.POST['lname'])
+    return redirect('/')
+def dojos(request):
+    dojo.objects.create(name=request.POST['name'], city=request.POST['city'], state=request.POST['state'], desc=request.POST['desc'])
+    return redirect('/')
