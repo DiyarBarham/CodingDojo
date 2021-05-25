696f781565fd1e144825806143edbf4fcec6c474
DiyarBarham <diyarbarham@gmail.com>
2021-05-25 17:14:09 +0300

    this is my answer for semi restful assignment

--- Python_Stack/django/django_fundamentals/django_intro/semi_restful/semi_restful_app/views.py
+++ Python_Stack/django/django_fundamentals/django_intro/semi_restful/semi_restful_app/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 from .models import Show
+from django.contrib import messages
+
 # Create your views here.
 def index(request):
     return redirect('/shows')
@@ -11,8 +13,25 @@ def show(request):
 def new(request):
     return render(request, 'new.html')
 def newrender(request):
-    title=  request.POST['title']
-    Network=  request.POST['network']
+    errors = Show.objects.basic_validator(request.POST)
+    if len(errors) > 0:
+        # if the errors dictionary contains anything, loop through each key-value pair and make a flash message
+        for key, value in errors.items():
+            messages.error(request, value)
+        # redirect the user back to the form to fix the errors
+        return redirect('/shows/new')
+    else:
+        # if the errors object is empty, that means there were no errors!
+        # retrieve the blog to be updated, make the changes, and save
+        show = Show.objects.get(id=id)
+        show.title = request.POST['title']
+        show.Network = request.POST['Network']
+        show.desc = request.POST['desc']
+        show.save()
+        messages.success(request, "Shows successfully updated")
+        # redirect to a success route
+    title=request.POST['title']
+    Network=request.POST['Network']
     release_date = request.POST['release_date']
     desc= request.POST['desc']
     data = Show.objects.create(title=title,Network=Network,release_date=release_date,desc=desc)
@@ -28,6 +47,22 @@ def edit(request, data):
     }
     return render(request, 'update.html', context)
 def editrender(request):
+    errors = Show.objects.basic_validator(request.POST)
+    if len(errors) > 0:
+        # if the errors dictionary contains anything, loop through each key-value pair and make a flash message
+        for key, value in errors.items():
+            messages.error(request, value)
+        # redirect the user back to the form to fix the errors
+        return redirect(f'/shows/{request.POST["hid"]}/edit')
+    else:
+        # if the errors object is empty, that means there were no errors!
+        # retrieve the blog to be updated, make the changes, and save
+        show = Show.objects.get(id=id)
+        show.title = request.POST['title']
+        show.Network = request.POST['Network']
+        show.desc = request.POST['desc']
+        show.save()
+        messages.success(request, "Shows successfully updated")
     title = request.POST['title']
     Network = request.POST['Network']
     release_date = request.POST['release_date']
