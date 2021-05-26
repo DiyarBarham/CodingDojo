3356462fabda584a3aad2b2b15aa87caca7bc9c0
DiyarBarham <diyarbarham@gmail.com>
2021-05-26 16:20:22 +0300

    this is my answer for login and registration assignment

--- Python_Stack/django/django_fundamentals/django_intro/users_auth/users_auth_app/models.py
+++ Python_Stack/django/django_fundamentals/django_intro/users_auth/users_auth_app/models.py
@@ -4,12 +4,12 @@ class UserManager(models.Manager):
     def basic_validator(self, postData):
         errors = {}
         # add keys and values to errors dictionary for each invalid field
-        if len(postData['firstname']) < 2:
+        if len(postData['firstname']) < 2 or not re.compile(r'^[a-zA-Z}+$'):
             errors["firstname"] = "first name should be at least 2 characters"
         if len(postData['lastname']) < 2:
             errors["lastname"] = "last name should be at least 2 characters"
 
-        if len(postData['passwd']) < 8:
+        if len(postData['passwd']) < 8 or postData['passwd']!=postData['confirmpass']:
             errors["password"] = "Password description should be at least 8 characters"
         EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
         if not EMAIL_REGEX.match(postData['email']):  # test whether a field matches the pattern
