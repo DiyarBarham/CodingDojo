65dd6778402fe82f8c5f7733b53ae0485a258aa9
DiyarBarham <diyarbarham@gmail.com>
2021-05-26 14:55:19 +0300

    this is my answer for login and registration assignment

--- Python_Stack/django/django_fundamentals/django_intro/users_auth/users_auth_app/models.py
+++ Python_Stack/django/django_fundamentals/django_intro/users_auth/users_auth_app/models.py
@@ -1,10 +1,27 @@
 from django.db import models
+import re
+class UserManager(models.Manager):
+    def basic_validator(self, postData):
+        errors = {}
+        # add keys and values to errors dictionary for each invalid field
+        if len(postData['firstname']) < 2:
+            errors["firstname"] = "first name should be at least 2 characters"
+        if len(postData['lastname']) < 2:
+            errors["lastname"] = "last name should be at least 2 characters"
 
+        if len(postData['passwd']) < 8:
+            errors["password"] = "Password description should be at least 8 characters"
+        EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+        if not EMAIL_REGEX.match(postData['email']):  # test whether a field matches the pattern
+            errors['email'] = "Invalid email address!"
+        return errors
 # Create your models here.
 class User(models.Model):
-    username = models.CharField(max_length=255)
+    firstname = models.CharField(max_length=255)
+    lastname = models.CharField(max_length=255)
     password = models.CharField(max_length=255)
     email = models.CharField(max_length=255)
     address = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    objects = UserManager()
--- Python_Stack/django/django_fundamentals/django_intro/users_auth/users_auth_app/views.py
+++ Python_Stack/django/django_fundamentals/django_intro/users_auth/users_auth_app/views.py
@@ -1,39 +1,46 @@
 from django.shortcuts import render, redirect, HttpResponse
 from .models import User
+from django.contrib import messages
+import bcrypt
+
 # Create your views here.
 def index(request):
     if 'user' in request.session:
         return redirect('/welcome')
     return render(request, 'index.html')
 def login(request):
-    username = request.POST['username']
-    password = request.POST['passwd']
-    for i in User.objects.all():
-        if i.username == username and i.password == password:
-            request.session['user']={
-                'username': username,
-                'password': password
-            }
+    user = User.objects.filter(email=request.POST['email'])
+    if user:
+        logged_user = user[0]
+        if bcrypt.checkpw(request.POST['passwd'].encode(), logged_user.password.encode()):
+            request.session['user'] = logged_user.email
             return redirect('/welcome')
     return redirect('/')
 
 def register(request):
-    username = request.POST['username']
+    errors = User.objects.basic_validator(request.POST)
+    # check if the errors dictionary has anything in it
+    if len(errors) > 0:
+        # if the errors dictionary contains anything, loop through each key-value pair and make a flash messagecopy
+        for key, value in errors.items():
+            messages.error(request, value)
+        # redirect the user back to the form to fix the errors
+        return redirect('/')
     password = request.POST['passwd']
-    address = request.POST['address']
-    email = request.POST['email']
-    request.session['user'] = {
-        'username': request.POST['username'],
-        'password': request.POST['passwd'],
-    }
-    User.objects.create(username=username, password=password, address=address, email=email)
+    pw_hash = bcrypt.hashpw(password.encode(), bcrypt.gensalt()).decode()
+    User.objects.create(firstname=request.POST['firstname'],lastname= request.POST['lastname'],  password=pw_hash, address = request.POST['address'], email = request.POST['email'])
+    request.session['user'] = request.POST['email']
+    messages.success(request, "User successfully created")
     return redirect('/welcome')
 def welcome(request):
     if 'user' not in request.session:
         return redirect('/')
-    username = request.session['user']['username']
-    return HttpResponse('Welcome')
+    user = User.objects.filter(email=request.session['user'])
+    context ={
+        'loggeduser': user[0]
+    }
+    return render(request, 'welcome.html', context)
 def logout(request):
-    if request.session['user']:
+    if 'user' in request.session:
         request.session.clear()
     return redirect('/')
