5e88b1ba310e95aaf4ddf8e12ed39102ca4a792d
DiyarBarham <diyarbarham@gmail.com>
2021-06-09 11:20:55 +0300

    firt assignment

---
+++ Java_Stack/Java_Fun/First Java Program/src/main.java
@@ -0,0 +1,12 @@
+
+public class main {
+
+	public static void main(String[] args) {
+		int age = 19;
+		String name = "Diyar Barham", adress = "Ramallah, Palestine";
+		System.out.println("My name is "+name);
+		System.out.printf("I am %d years old%n",age);
+		System.out.println("My hometown is "+adress);
+	}
+
+}
