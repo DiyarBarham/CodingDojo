4210ea86c261759ad9b77e1a3ec3864c9306fb88
DiyarBarham <diyarbarham@gmail.com>
2021-06-09 11:50:21 +0300

    answer for second assignment

--- Java_Stack/Java_Fun/Pythagorean Theorem/src/Main.java
+++ Java_Stack/Java_Fun/Pythagorean Theorem/src/Main.java
@@ -3,18 +3,20 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		int i = 0;
-		while (i == 0) {
 		Scanner scan = new Scanner(System.in);
+		char i = 'y';
+		while (i == 'y') {
 		System.out.println("enter the first length:");
 		int val1 = scan.nextInt();
 		System.out.println("enter the second length:");
 		int val2 = scan.nextInt();
 		double result = Pyth(val1, val2);
 		System.out.println("the length of the Hypotenuse is: "+result);
-		System.out.println("if want to continue enter 0");
-		i = scan.nextInt();
+		System.out.println("Do you want to continue? (y/n)");
+		i = scan.next().charAt(0);
 		}
+		scan.close();
+		System.out.println("Thanks for using our app :)");
 	}
 	static double Pyth(double val1, double val2) {
 		val1 = val1*val1;
