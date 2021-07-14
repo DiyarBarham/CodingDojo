d08c59512b78e29f098003bfd37c8476a63ff56f
DiyarBarham <diyarbarham@gmail.com>
2021-07-14 11:08:23 +0300

    mern stack

--- Mern_Stack/Mern/propitup/src/component/PersonCard.js
+++ Mern_Stack/Mern/propitup/src/component/PersonCard.js
@@ -25,13 +25,19 @@ constructor(props) {
     super(props);
 
     this.state = {
+        age: this.props.age
     };
+
 }
+    increase=()=>{
+        this.setState({age:this.state.age+1});
+    }
 
     render() {
         return <div><h1>{this.props.lastname}, {this.props.firstname}</h1>
-                <p>Age: {this.props.age}</p>
+                <p>Age: {this.state.age}</p>
                 <p>Hair Color: {this.props.haircolor}</p>
+                <button onClick={this.increase}>Birthday Button for {this.props.firstname} {this.props.lastname}</button>
         </div>;
     }
 }
