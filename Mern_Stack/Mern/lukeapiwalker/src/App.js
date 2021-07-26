bb93b77688ff755fb41ad1b8a89d331832d50404
DiyarBarham <diyarbarham@gmail.com>
2021-07-26 16:32:21 +0300

    mern stack

--- Mern_Stack/Mern/lukeapiwalker/src/App.js
+++ Mern_Stack/Mern/lukeapiwalker/src/App.js
@@ -8,20 +8,25 @@ function App() {
     const [name, setname] = useState("people");
     const [id, setid] = useState(""); 
     const [responseData, setResponseData] = useState([]);
+    const [keys, setkeys] = useState([]);
     const findd = (e) => {
       e.preventDefault();
       axios.get('https://swapi.dev/api/'+name+'/'+id)
       .then(response=>{setResponseData(response.data)})
       navigate('/'+name+'/'+id);
     };
+    axios.get('https://swapi.dev/api/')
+      .then(response=>{setkeys(response.data)})    
   return (
     <div className="App">
         <form onSubmit={ findd }>
             <div>
                 <label>Search for: </label>
                 <select onChange={ (e) => setname(e.target.value)} >
-                    <option value="people">People</option>
-                    <option value="planets">Planets</option>
+                    {
+            Object.keys(keys).map( (key, index) => 
+                <option value={ key }>{ key }</option>
+            )}
                 </select>
                 <label> ID: </label> 
                 <input type="text" onChange={ (e) => setid(e.target.value) } />
