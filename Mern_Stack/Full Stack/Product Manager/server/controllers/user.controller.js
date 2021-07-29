3c1e4e9f115e0d80cfd2547f971b2938495bb644
DiyarBarham <diyarbarham@gmail.com>
2021-07-29 17:15:38 +0300

    Product Manager 2 assignment

--- Mern_Stack/Full Stack/Product Manager/server/controllers/user.controller.js
+++ Mern_Stack/Full Stack/Product Manager/server/controllers/user.controller.js
@@ -5,4 +5,13 @@ module.exports.createNewProduct = (req, res) => {
     .then(newlyCreatedProduct => res.json({ Product: newlyCreatedProduct }))
     .catch(err => res.status(400).json({ message: "Something went wrong", error: err }));
 };
-
+module.exports.findAllProducts = (req, res) => {
+  Product.find()
+    .then(all => res.json({ products: all }))
+    .catch(err => res.json({ message: "Something went wrong", error: err }));
+};
+module.exports.findProduct = (req, res) => {
+	Product.findOne({ _id: req.params.id })
+		.then(findProduct => res.json({ product: findProduct }))
+		.catch(err => res.json({ message: "Something went wrong", error: err }));
+};
--- Mern_Stack/Full Stack/Product Manager/server/routes/user.routes.js
+++ Mern_Stack/Full Stack/Product Manager/server/routes/user.routes.js
@@ -2,4 +2,6 @@ const UserController = require("../controllers/user.controller");
 
 module.exports = app => {
   app.post("/api/products/new", UserController.createNewProduct);
+  app.get("/api/products/all", UserController.findAllProducts);
+  app.get("/api/products/:id", UserController.findProduct);
 };
